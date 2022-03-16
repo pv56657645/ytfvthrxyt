2d7f8aed067744caf4c81ea2c4b48b8a94c2fddc
pv56657645 <101644526+pv56657645@users.noreply.github.com>
2022-03-16 14:18:02 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import telebot
+import random
+
+yes_nol = ["Да", "Нет"]
+
+tokin = '1749862558:AAEueIJsfOQ1FWlNDGtP5BunzAq77UsH7bg'
+
+bot = telebot.TeleBot(tokin)
+
+@bot.message_handler(commands=["start"])
+def start(m, res=False):
+    bot.send_message(m.chat.id, 'Я великй стасик хуясик, я отвечу на все твои вопросы!')
+
+@bot.message_handler(content_types=['text'])
+def handle_text(message):
+        bot.send_message(message.chat.id, random.choice(yes_nol))
+
+bot.polling(none_stop=True, interval=0)
